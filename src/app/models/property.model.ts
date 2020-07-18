78306e52ce7a3b58d91297e1630ee8c1b45b82fc
aleksandar.krstic <aleksandarkrstic1992@gmail.com>
2020-07-18 13:38:52 +0200

    Changing property view on booking

--- src/app/models/property.model.ts
+++ src/app/models/property.model.ts
@@ -1,14 +1,7 @@
 export class Property {
-    category: string;
-    categoryTitle: string;
     distance: number;
-    highlightedTitle: string;
-    highlightedVicinity: string;
-    href: string;
     id: string;
     position: number[];
-    resultType: string;
     title: string;
     type: string;
-    vicinity: string;
 }
