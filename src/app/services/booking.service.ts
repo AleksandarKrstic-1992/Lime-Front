55a620aeeae42543b347cc2750b20023e731318b
aleksandar.krstic <aleksandarkrstic1992@gmail.com>
2020-07-19 19:56:04 +0200

    Pass datetime as string

--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -11,8 +11,20 @@ export class BookingService extends BaseService {
     }
 
     addBooking(propertyId, callback) {
-        const dto: Booking = new Booking(propertyId);
+        const dto: Booking = new Booking(propertyId, this.getDatetimeAsString());
         const endpoint = `${this.baseUrl}${Consts.bookingPath}`;
         this.post(endpoint, dto, callback);
+    }
+
+    private getDatetimeAsString(): string {
+        const date = new Date();
+        const dateStr =
+            ('00' + (date.getMonth() + 1)).slice(-2) + '/' +
+            ('00' + date.getDate()).slice(-2) + '/' +
+            date.getFullYear() + ' ' +
+            ('00' + date.getHours()).slice(-2) + ':' +
+            ('00' + date.getMinutes()).slice(-2) + ':' +
+            ('00' + date.getSeconds()).slice(-2);
+        return dateStr;
     }
 }
